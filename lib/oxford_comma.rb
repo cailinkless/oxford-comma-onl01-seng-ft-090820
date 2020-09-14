79ee3e41fe9834acce5764220685532603e85156
cailinkless <cailin.nysff@gmail.com>
2020-09-14 00:37:22 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.length < 2
+        return "#{array[0]}"
+    elsif array.length == 2
+        return "#{array[0]} and #{array[1]}"
+    else
+        last_item = []
+        last_item << "and #{array.pop}"
+        array << last_item
+        array.join(", ")
+    end
 end
